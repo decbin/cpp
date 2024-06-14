fafcd465bb0c2475c63ce91d70d4a91c6b1ba65d
db.s <decbin@binnala.com>
2024-06-14 18:06:30 +0900

    cmakeでビルド、デバックできるようにする

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
+using namespace std::string_literals;
 
 int main() {
-    std::cout << "hello world" << std::endl;
-    std::cout << "test" << std::endl;
+    std::cout << "hello world14" << std::endl;
+    std::cout << "testss"s << std::endl;
 }
